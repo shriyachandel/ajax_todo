c8ae81119e1f6bc7d2552ade721f33df1e8a6476
shriya <shriyachandel1998@gmail.com>
2024-04-11 07:01:11 +0530

    update code

--- header.php
+++ header.php
@@ -9,5 +9,7 @@
   <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  <script src="assets/custom-js.js"></script>
 </head>
 <body>
