58b0be3c0d26a617f1cf498a060fc4b624ec5aa7
shriya <shriyachandel1998@gmail.com>
2024-04-11 07:10:02 +0530

    update code

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
         <input type="text" class="form-control mb-2 mr-sm-2" id="name" placeholder="Enter name" name="Name">
         <label for="description" class="mb-2 mr-sm-2">Description:</label>
         <input type="text" class="form-control mb-2 mr-sm-2" id="description" placeholder="Enter description" name="Description">
-        <button type="submit" class="btn btn-primary mb-2">Add Item</button>
+        <button type="submit" class="btn btn-primary mb-2" id="add_item">Add Item</button>
       </form>
     </div>
     <div class="col-lg-12">
@@ -54,9 +54,11 @@
   <div>
 </div>
 <script>
-
-  var form_inputs = ['#cus_full_name', '#cus_email', '#cus_address', '#phone_no'];
+  $('#add_item').on('click',function(){
+    var form_inputs = ['#cus_full_name', '#cus_email', '#cus_address', '#phone_no'];
   var formData = new FormData(jQuery("#get_estimate_form")[0]);
+  });
+  
 </script>
 
 <?php include('footer.php'); ?> 
