1b4bbf9bd598c750951188d68bb3050290553fe8
shriya <shriyachandel1998@gmail.com>
2024-04-15 07:05:48 +0530

    update code

--- save.php
+++ save.php
@@ -1,5 +1,14 @@
 <?php
-if(isset($_POST['name'])){
- echo $_POST['name'];
+// Check if any data was sent via POST
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Check if any data is set in the $_POST array
+    if (!empty($_POST)) {
+        // If there is data, print all of it
+        print_r($_POST);
+    } else {
+        echo "No data received.";
+    }
+} else {
+    echo "This endpoint only accepts POST requests.";
 }
 ?>
