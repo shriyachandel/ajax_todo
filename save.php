74974c4e91ac0087ef253fdf129c308f81533f7c
shriya <shriyachandel1998@gmail.com>
2024-04-16 07:09:31 +0530

    update code

---
+++ save.php
@@ -0,0 +1,23 @@
+<?php include('db/db.php') ?>
+<?php
+// Check if any data was sent via POST
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Check if any data is set in the $_POST array
+    if (!empty($_POST)) {
+        $name = $_POST['Name'];
+        $description = $_POST['Description'];
+        $sql = "INSERT INTO items(Name,Description) VALUES ('$name','$description')";
+        $result = mysqli_query($conn,$sql);
+        if($result){
+            echo json_encode(array("statusCode"=>200));
+        }else{
+            echo json_encode(array("statusCode"=>201));
+        }
+    } 
+    else {
+        echo "No data received.";
+    }
+} else {
+    echo "This endpoint only accepts POST requests.";
+}
+?>
